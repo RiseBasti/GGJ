64d3187354cadd98b5496924e574796ac5af6b6b
Paskull <felber93@hotmail.com>
2018-01-26 23:53:51 +0100

    enemies

---
+++ GGJ_Transmission/Assets/Scripts/Enemies.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemies : MonoBehaviour {
+
+    //Eigenschaften
+    public string EmemyTyp = "";
+    public float EnemiesSize = 1;
+    public int EnemyHP = 1;
+    public float EnemySpeed = 0.01f;
+    private Rigidbody2D rb;
+    public Transform EnemyTarget;
+
+    //Speed
+    public float MaxVelocity = 0.5f;
+    public float MinVelocity = -0.5f;
+    public bool boost = true;
+
+    // Use this for initialization
+    void Start () {
+
+        rb = GetComponent<Rigidbody2D>();
+       Rotation();
+
+
+    }
+
+
+
+    // Update is called once per frame
+    void Update () {
+
+        Speed();
+
+
+        Detection();
+
+    }
+
+
+    void Rotation() {
+
+        float AngleRad = Mathf.Atan2(EnemyTarget.position.y - transform.position.y, EnemyTarget.position.x - transform.position.x);
+        float AngleDeg = (180 / Mathf.PI) * AngleRad;
+        this.transform.rotation = Quaternion.Euler(0, 0, AngleDeg);
+
+
+    }
+    void Detection() {
+
+        float d = Vector2.Distance(EnemyTarget.position, transform.position);
+
+        if (d < 1) {
+
+            Rotation();
+
+        }
+    
+    }
+
+    void Speed() {
+
+        Debug.Log(boost);
+
+        float RVX = rb.velocity.x;
+        float RVY = rb.velocity.y;
+
+        if (RVX > 1 || RVX < -1) {
+
+           //boost = false;
+           //rb.velocity = new Vector2(0, RVY);
+        }
+
+        if (RVY > 1 || RVY < -1)
+        {
+
+           // boost = false;
+           // rb.velocity = new Vector2(RVX, 0);
+        }
+
+        if (boost) {
+          
+           // rb.AddForce(transform.right * EnemySpeed);
+
+        }
+  
+
+
+        
+        
+        
+
+
+    }
+}
---
+++ GGJ_Transmission/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+
+
+
+
+
+    public GameObject Enemy;
+    public GameObject Camera;
+    public float Timer = 120;
+    private float CountDown = 1;
+    float timer = 120;
+
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        
+
+         timer -= 1;
+
+        if (timer <= 0) {
+            Spawner();
+            timer = 120;
+
+        }
+
+
+
+       
+
+
+    }
+
+
+    void Spawner() {
+
+
+        float SpawnArealXmin = Camera.transform.position.x - 10;
+        float SpawnArealXmax = Camera.transform.position.x + 10;
+
+        float SpawnArealYmin = Camera.transform.position.y - 10;
+        float SpawnArealYmax = Camera.transform.position.y + 10;
+
+        float RandomX = Random.Range(SpawnArealXmin, SpawnArealXmax);
+        float RandomY = Random.Range(SpawnArealYmin, SpawnArealYmax);
+
+        Vector2 SpawnAreal = new Vector2(RandomX, RandomY);
+
+        Instantiate(Enemy, SpawnAreal,transform.rotation);
+
+    }
+}
