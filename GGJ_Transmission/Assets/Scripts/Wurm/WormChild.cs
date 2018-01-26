f84aacb4500cc8c3303b851b0ff430bb4b0f7636
RiseBasti <romanvonrotz@outlook.com>
2018-01-26 23:52:55 +0100

    worm movement

---
+++ GGJ_Transmission/Assets/Scripts/Wurm/WormChild.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WormChild : MonoBehaviour
+{
+    //Variables
+    //Stats
+    //...
+
+    //Script
+    [HideInInspector] public float speed;
+    [HideInInspector] public float distMin;
+    [HideInInspector] public float distMax;
+    private Vector2 direction = new Vector2(0,0);
+    private float rotation = 0;
+
+    //Objects
+    [HideInInspector] public GameObject parent;
+
+    //Components
+    private Rigidbody2D rb;
+
+
+	// Use this for initialization
+	private void Start ()
+    {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	private void Update ()
+    {
+        Move();
+	}
+
+    private void Move()
+    {
+        direction = (new Vector2(parent.transform.position.x - transform.position.x, parent.transform.position.y - transform.position.y)).normalized;
+        rotation = Mathf.Atan2(parent.transform.position.y - transform.position.y, parent.transform.position.x - transform.position.x) * Mathf.Rad2Deg;
+        float distance = Vector2.Distance(parent.transform.position, transform.position);
+
+        transform.rotation = Quaternion.Euler(0f, 0f, rotation);
+
+        if(distance>distMax)
+        {
+            rb.MovePosition(rb.position + direction * speed * Time.deltaTime);
+        }
+        else if (distance>distMin)
+        {
+            rb.MovePosition(rb.position + direction * ((distance-distMin)/(distMax-distMin)) * speed * Time.deltaTime);
+        }
+    }
+}
