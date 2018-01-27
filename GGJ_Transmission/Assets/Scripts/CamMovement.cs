7fb4fdcff7bb737ac7a9e70ff79f893fa77bf618
RiseBasti <romanvonrotz@outlook.com>
2018-01-27 16:55:55 +0100

    cammovement

---
+++ GGJ_Transmission/Assets/Scripts/CamMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamMovement : MonoBehaviour
+{
+    //Variables
+    public GameObject worm;
+    public GameObject zelle;
+
+
+	// Use this for initialization
+	private void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	private void Update ()
+    {
+        transform.position = new Vector3((worm.transform.position.x + zelle.transform.position.x) / 2, (worm.transform.position.y + zelle.transform.position.y) / 2,-10);
+	}
+}
